d4c21a1a9de6eda0f1b7c339196525c7846de0f0
Bane34 <bane34.42@gmail.com>
2023-02-16 17:45:50 +0100

    Implementacion del metodo de Horner

---
+++ eval_polinomio.m
@@ -0,0 +1,15 @@
+% Evaluacion de polinomios mediante el metodo de Horner
+function val = eval_polinomio(coeficientes, val)
+    % Suponemos que hay mas de 1 coeficiente con dimension 1xn
+    % Los vectores seran de la forma: 
+    % [a_n, a_n-1, ..., a_0] donde a_n * x^n + a_n-1 * x^n-1 + ··· + a_0
+
+    n = length(coeficientes);
+    q = coeficientes(1);
+
+    for i = 2:n
+        q = q * val + coeficientes(i);
+    end
+    
+    val = q;
+end
