ce2b7066006dc41de5602c56c85b7e0ad2355211
Bane34 <bane34.42@gmail.com>
2023-02-23 10:34:06 +0100

    Añadido Plots.m

--- Practica 1/Plots.m
+++ Practica 1/Plots.m
@@ -1,8 +1,20 @@
-function Plots()
-    nodos = [-1:0.5:1];
-    t = Funcion1(nodos);
-    r = Funcion2(nodos);
 
-    plot(nodos, t);
-    plot(nodos, r);
+nodos = [-2:0.1:2];
+t = Funcion1(nodos);
+r = Funcion2(nodos);
+
+% Pintar dos funciones al mismo tiempo
+plot(nodos, t);
+hold on;
+plot(nodos, r);
+
+% Pintar las funciones una encima de otra
+subplot(2,3,1), plot(nodos, t);
+subplot(2,3,5), plot(nodos, r);
+
+resultados(:,1) = t;
+resultados(:,2) = r;
+
+for i = 1:6
+    subplot(2, 3, i), plot(nodos, resultados, 'r--'); % `r--` -> pintar discontinuo y en rojo
 end
