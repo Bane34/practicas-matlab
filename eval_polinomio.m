041757bf32f8ac2964926b188d3fb17b4152315a
Bane34 <bane34.42@gmail.com>
2023-02-23 15:32:53 +0100

    Modificado para aceptar vectores de valores

--- eval_polinomio.m
+++ eval_polinomio.m
@@ -1,5 +1,5 @@
 % Evaluacion de polinomios mediante el metodo de Horner
-function val = eval_polinomio(coeficientes, val)
+function c = eval_polinomio(coeficientes, val)
     % Suponemos que hay mas de 1 coeficiente con dimension 1xn
     % Los vectores seran de la forma: 
     % [a_n, a_n-1, ..., a_0] donde a_n * x^n + a_n-1 * x^n-1 + ··· + a_0
@@ -8,8 +8,8 @@ function val = eval_polinomio(coeficientes, val)
     q = coeficientes(1);
 
     for i = 2:n
-        q = q * val + coeficientes(i);
+        q = val.*q + coeficientes(i);
     end
     
-    val = q;
+    c = q;    
 end
